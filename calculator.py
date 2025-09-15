aa3f28ecc0eeca85e8b1c5efe8cc5cbf9d18f07e
Elizaveta Chegodaeva <71721620+lizavetaChe@users.noreply.github.com>
2025-09-15 20:24:34 +0300

    first_commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = int(input("a = "))
+b = int(input("b = "))
+op = input("Доступные операции (+, -, *, /) : ")
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b)
+else:
+    print("Неверная операция")
