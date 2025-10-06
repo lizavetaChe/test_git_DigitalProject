44692d7c50aea454fe99727b8b786a891567723b
safliz <lizok.yatut@yandex.ru>
2025-10-06 19:12:05 +0300

    Fix

--- hehe.py
+++ hehe.py
@@ -1,1 +1,10 @@
+def helloworld(s):
+    if s=='print':
+        print('hello world')
+
+def bitcoin (btc):
+    print(f'{btc} BTC = {btc*10138857} руб')
+
+
 helloworld('print')
+bitcoin(float(input('число BTC: ')))
