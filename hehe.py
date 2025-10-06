319eec2ac0ec92e84eb583af22329fb42a9a365d
lilybezrogova <lilybezrogova@list.ru>
2025-10-06 14:26:36 +0000

    Bezrogova

--- hehe.py
+++ hehe.py
@@ -1,1 +1,13 @@
+import datetime
+
+def helloworld(p):
+    if p == 'print':
+        print("Привет!")
+    elif p == 'time':
+        print(f"Текущее время: {datetime.datetime.now().strftime('%H.%M.%S')}")
+    elif p == 'date':
+        print(f"Текущая дата: {datetime.datetime.today().strftime('%d-%m-%Y')}")
+    else:
+        print(f"Ошибка: {p}")
+
 helloworld('print')
