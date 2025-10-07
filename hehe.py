3a1a1e508fa7a520de22b6d0a7f1a55dc52ea18e
Daniil Sulin <d.nertyco@gmail.com>
2025-10-07 13:47:15 +0300

    Исправил функцию helloworld и добавил выбор режима

--- hehe.py
+++ hehe.py
@@ -1,1 +1,15 @@
+def helloworld(mode='print'):
+    message = "Hello, world!"
+    if mode == 'print':
+        print(message)
+    elif mode == 'return':
+        return message
+    else:
+        print("Неизвестный режим:", mode)
+
 helloworld('print')
+
+user_mode = input("Введите режим (print / return): ")
+result = helloworld(user_mode)
+if result:
+    print("Функция вернула:", result)
